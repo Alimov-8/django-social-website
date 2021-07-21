6d954b18053901f556475024f50947b9b7c11d0a
Alimov-8 <alimov.abdullokh8@gmail.com>
2021-07-21 13:15:54 +0500

    like|AJAX actions with jQuery

--- images/views.py
+++ images/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
+
 
 from .forms import ImageCreateForm 
 from .models import Image
@@ -42,5 +45,25 @@ def image_detail(request, id, slug):
                     'images/image/detail.html',
                     {'section': 'images', 
                     'image': image})
+
+
+@login_required
+@require_POST
+def image_like(request):
+    image_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if image_id and action:
+        try:
+            image = Image.objects.get(id=image_id)
+            if action == "like":
+                image.users_like.add(request.user)
+            else:
+                image.users_like.remove(request.user)
+            return JsonResponse({'status':'ok'})
+        except:
+            pass
+    
+    return JsonResponse({'status':'error'})
+
 
 
