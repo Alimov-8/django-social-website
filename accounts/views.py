d58cd78c71d2cd47e7589a374771f5b94b8cec43
Alimov-8 <alimov.abdullokh8@gmail.com>
2021-07-23 18:16:16 +0500

    Building AJAX view to follow users

--- accounts/views.py
+++ accounts/views.py
@@ -4,9 +4,13 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
+from common.decorators import ajax_required
+
 
 from django.contrib.auth.models import User
-from .models import Profile
+from .models import Profile, Contact
 from .forms import LoginForm, UserRegistrationForm, \
                     UserEditForm, ProfileEditForm
 
@@ -110,3 +114,27 @@ def user_detail(request, username):
                   'accounts/user/detail.html',
                   {'section': 'people',
                    'user': user})
+
+
+@ajax_required
+@require_POST
+@login_required
+def user_follow(request):
+    user_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if user_id and action:
+        try:
+            user = User.objects.get(id=user_id)
+            if action == 'follow':
+                Contact.objects.get_or_create(
+                            user_from=request.user,
+                            user_to=user)
+            else:
+                Contact.objects.filter(user_from=request.user,
+                                user_to=user).delete()
+            return JsonResponse({'status':'ok'})
+
+        except User.DoesNotExist:
+            return JsonResponse({'status':'error'})
+    
+    return JsonResponse({'status':'error'})
