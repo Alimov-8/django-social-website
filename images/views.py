4563f6135d761588cec56e769d003233fbda92f0
Alimov-8 <alimov.abdullokh8@gmail.com>
2021-07-26 16:04:39 +0500

    Storing a ranking in Redis

--- images/views.py
+++ images/views.py
@@ -49,8 +49,12 @@ def image_create(request):
 
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
+
     # increment total image views by 1
     total_views = r.incr(f'image:{image.id}:views') #object-type:id:field
+    # increment image ranking by 1
+    r.zincrby('image_ranking', 1, image.id)
+
     return render(request,
                     'images/image/detail.html',
                     {'section': 'images', 
@@ -110,3 +114,17 @@ def image_list(request):
 r = redis.Redis(host=settings.REDIS_HOST,
                 port=settings.REDIS_PORT,
                 db=settings.REDIS_DB)
+
+
+@login_required
+def image_ranking(request):
+    # get image ranking dictionary
+    image_ranking = r.zrange('image_ranking', 0, -1,
+                            desc=True)[:10]
+    image_ranking_ids = [int(id) for id in image_ranking]
+    # get most viewed images
+    most_viewed = list(Image.objects.filter(id__in=image_ranking_ids))
+    most_viewed.sort(key=lambda x: image_ranking_ids.index(x.id))
+    return render(request,'images/image/ranking.html',
+                {'section': 'images',
+                'most_viewed': most_viewed})
